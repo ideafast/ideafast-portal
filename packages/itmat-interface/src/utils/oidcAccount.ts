7f73f26af0506c363a1754e8e4c061bf618e4640
GuanyuTao <guanyu.tao@outlook.com>
2022-03-09 16:34:57 +0000

    odic account: find account by given account id

---
+++ packages/itmat-interface/src/utils/oidcAccount.ts
@@ -0,0 +1,20 @@
+import { db } from '../database/database';
+import {UserInputError} from "apollo-server-express";
+
+export class Account {
+    static async findAccount(ctx, id) {
+        const result = await db.collections!.users_collection.findOne({ deleted: null, id: id });
+        if (!result) {
+            throw new UserInputError('User does not exist.');
+        }
+        return {
+            accountId: id,
+            async claims() {
+                return {
+                    sub: id,
+                    email: result.email,
+                };
+            },
+        };
+    }
+}
